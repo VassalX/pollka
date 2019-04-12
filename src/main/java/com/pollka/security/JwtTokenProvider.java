3b71ae81087516885a5d28853caf7125d14f3e0b
Vasylenko Olexandr <olexandr.vasylenko257@gmail.com>
2019-04-13 00:28:11 +0300

    Security configuration

---
+++ src/main/java/com/pollka/security/JwtTokenProvider.java
@@ -0,0 +1,56 @@
+package com.pollka.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication){
+        UserInfo userInfo = (UserInfo) authentication.getPrincipal();
+
+        Date now_date = new Date();
+        Date expire_date = new Date(now_date.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userInfo.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expire_date)
+                .signWith(SignatureAlgorithm.ES512, jwtSecret)
+                .compact();
+    }
+
+    public Long getUserIdFromJWT(String tok){
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(tok)
+                .getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e){
+            logger.error(e.getMessage());
+        }
+        return false;
+    }
+}
