610340d0104c3f2565ed9fc45342d4ae60528f28
Kate-Dt <dotsiuk.k@gmail.com>
2019-04-13 18:25:49 +0300

    Added controllers, response classes

--- src/main/java/com/pollka/config/AuditingConfig.java
+++ src/main/java/com/pollka/config/AuditingConfig.java
@@ -1,10 +1,39 @@
 package com.pollka.config;
 
+import com.pollka.security.UserInfo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
 
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new SpringSecurityAuditAwareImpl();
+    }
+}
+
+class SpringSecurityAuditAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        UserInfo userPrincipal = (UserInfo) authentication.getPrincipal();
+
+        return Optional.ofNullable(userPrincipal.getId());
+    }
 }
---
+++ src/main/java/com/pollka/controller/UserController.java
@@ -0,0 +1,86 @@
+package com.pollka.controller;
+
+import com.pollka.exception.ResourceNotFoundException;
+import com.pollka.model.User;
+import com.pollka.repository.PollRepository;
+import com.pollka.repository.UserRepository;
+import com.pollka.repository.VoteRepository;
+import com.pollka.requests.*;
+import com.pollka.security.UserInfo;
+import com.pollka.service.PollService;
+import com.pollka.security.CurrentUser;
+import com.pollka.util.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserInfo currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        long pollCount = pollRepository.countByCreatedBy(user.getId());
+        long voteCount = voteRepository.countByUserId(user.getId());
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt(), pollCount, voteCount);
+
+        return userProfile;
+    }
+
+    @GetMapping("/users/{username}/polls")
+    public PagedResponse<PollResponse> getPollsCreatedBy(@PathVariable(value = "username") String username,
+                                                         @CurrentUser UserInfo currentUser,
+                                                         @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                         @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getPollsCreatedBy(username, currentUser, page, size);
+    }
+
+
+    @GetMapping("/users/{username}/votes")
+    public PagedResponse<PollResponse> getPollsVotedBy(@PathVariable(value = "username") String username,
+                                                       @CurrentUser UserInfo currentUser,
+                                                       @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                       @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getPollsVotedBy(username, currentUser, page, size);
+    }
+
+}
